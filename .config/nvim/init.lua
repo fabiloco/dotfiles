59973d9fc1c64393c9233e437e77032ff3b1ae5d
Fabián Sánchez <faalsaru@gmail.com>
2023-02-01 22:17:21 -0500

    new snippets, format on save and powermenu in polybar

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,12 @@
-require "alucard.options"
-require "alucard.plugins"
-require "alucard.cmp"
-require "alucard.lsp"
-require "alucard.treesitter"
-require "alucard.keymaps"
-require "alucard.alpha"
-require "alucard.comment"
-require "alucard.nvim-tree"
-require "alucard.telescope"
-require "alucard.lualine"
-require "alucard.bufferline"
-
-
+require("alucard.options")
+require("alucard.plugins")
+require("alucard.cmp")
+require("alucard.lsp")
+require("alucard.treesitter")
+require("alucard.keymaps")
+require("alucard.alpha")
+require("alucard.comment")
+require("alucard.nvim-tree")
+require("alucard.telescope")
+require("alucard.lualine")
+require("alucard.bufferline")
--- .config/nvim/lua/alucard/telescope.lua
+++ .config/nvim/lua/alucard/telescope.lua
@@ -3,7 +3,24 @@ if not status_ok then
   return
 end
 
+local status_ok_telescope, telescope = pcall(require, 'telescope')
+if not status_ok_telescope then
+  return
+end
+
+telescope.setup {
+  defaults = {
+    file_ignore_patterns = { "node_modules" }
+  }
+}
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+vim.keymap.set('n', '<leader>ft', builtin.treesitter, {})
+
+vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
+vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
+vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
